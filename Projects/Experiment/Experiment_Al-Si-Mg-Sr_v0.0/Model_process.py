572b9d433408987b5bf94043e09358d6dd4a392b
aguang5241 <759229100@qq.com>
2020-04-06 23:03:31 +0800

    update

--- Projects/Experiment/Experiment_Al-Si-Mg-Sr_v0.0/Model_process.py
+++ Projects/Experiment/Experiment_Al-Si-Mg-Sr_v0.0/Model_process.py
@@ -2,6 +2,7 @@ import os
 
 import matplotlib
 import matplotlib.pyplot as plt
+import brokenaxes
 import numpy as np
 import pandas as pd
 import seaborn as sns
@@ -111,45 +112,72 @@ def main(parameters_list):
 
     # 绘制散点图
 
-    def draw_scatter(x_training, y_training, x_predicting, y_predicting, item, training_accuracy, testing_accuracy, error):
-        if item == 'UTS / MPa':
-            fig_name = 'UTS'
-            ymin = 190
-            ymax = 220
-            e = error[0]
-        elif item == 'YS / MPa':
-            fig_name = 'YS'
-            # 6data
-            # ymin = 85
-            # ymax = 102
-            # 7data
-            ymin = 80
-            ymax = 110
-            e = error[1]
-        else:
-            fig_name = 'EL'
-            ymin = 7.5
-            ymax = 16
-            e = error[2]
+    def draw_scatter(x_training, y_training, x_predicting, y_predicting, error):
+        # 7data
+        ymin1 = 85
+        ymax1 = 240
+        ymin2 = 7
+        ymax2 = 18
+        # 6data
+        ymin1 = 80
+        ymax1 = 250
+        ymax2 = 20
+
         sns.set(font="Times New Roman", font_scale=1.3, style='ticks')
         matplotlib.rcParams['xtick.direction'] = 'in'
         matplotlib.rcParams['ytick.direction'] = 'in'
         fig = plt.figure(figsize=(8, 6))
-        ax = plt.subplot()
-        ax.set_xlabel('Sr / wt. %')
-        ax.set_ylabel(item)
-        ax.vlines(0.005, ymin, ymax, linestyles='dashed', color='r')
-        ax.vlines(0.075, ymin, ymax, linestyles='dashed', color='r')
-        ax.text(0.09, ymin + (ymax - ymin) * 0.1, 'Accuracy: %.2f %%' %
-                (training_accuracy * 100))
-        ax.text(0.01, ymax, 'w(Sr) = 0.005', color='r')
-        ax.text(0.06, ymax, 'w(Sr) = 0.075', color='r')
-        ax.scatter(x_predicting, y_predicting, label='Predicting data')
-        ax.errorbar(x_training, y_training, yerr=e, linestyle='None',
-                    capsize=5, ecolor='#dd8452')
-        ax.scatter(x_training, y_training, s=50, label='Training data')
-        ax.legend(loc='upper right', frameon=False)
-        plt.savefig(path + 'elements_%s.png' % fig_name)
+        ax1 = plt.subplot()
+        ax2 = ax1.twinx()
+        ax1.set_xlabel('Sr / wt. %')
+        ax1.set_ylabel('Strength / MPa')
+        ax1.set_ylim(ymin1, ymax1)
+        ax2.set_ylabel('Elongation / %', color='chocolate')
+        ax2.spines['right'].set_color('chocolate')
+        ax2.yaxis.label.set_color('chocolate')
+        ax2.tick_params(axis='y', colors='chocolate')
+        ax2.set_ylim(ymin2, ymax2)
+
+        # UTS
+        pl11 = ax1.scatter(x_predicting, y_predicting[:, 0],
+                           s=15, color='cornflowerblue')
+        ax1.errorbar(x_training, y_training[:, 0], yerr=error[0], linestyle='None',
+                     capsize=5, ecolor='royalblue')
+        pl12 = ax1.scatter(
+            x_training, y_training[:, 0], s=25, color='royalblue')
+        # YS
+        pl21 = ax1.scatter(x_predicting, y_predicting[:, 1],
+                           s=15, color='mediumseagreen')
+        ax1.errorbar(x_training, y_training[:, 1], yerr=error[1], linestyle='None',
+                     capsize=5, ecolor='green')
+        pl22 = ax1.scatter(x_training, y_training[:, 1], s=25, color='green')
+        # EL
+        pl31 = ax2.scatter(x_predicting, y_predicting[:, 2],
+                           s=15, color='chocolate')
+        ax2.errorbar(x_training, y_training[:, 2], yerr=error[2], linestyle='None',
+                     capsize=5, ecolor='saddlebrown')
+        pl32 = ax2.scatter(
+            x_training, y_training[:, 2], s=25, color='saddlebrown')
+
+        ax1.vlines(0.005, ymin1, ymax1, linestyles='dashed',
+                   color='r', linewidth=2)
+        ax1.vlines(0.075, ymin1, ymax1, linestyles='dashed',
+                   color='r', linewidth=2)
+        ax1.text(0.01, 130, 'w(Sr) = 0.005', color='r')
+        ax1.text(0.08, 130, 'w(Sr) = 0.075', color='r')
+
+        label11 = 'Predicted UTS'
+        label12 = 'Experimental UTS'
+        label21 = 'Predicted YS'
+        label22 = 'Experimental YS'
+        label31 = 'Predicted EL'
+        label32 = 'Experimental EL'
+
+        plt.legend([pl11, pl21, pl31, pl12, pl22, pl32],
+                   [label11, label21, label31, label12, label22, label32],
+                   loc='upper right', frameon=False, ncol=2)
+        plt.savefig(path + 'performance.png')
+
         # plt.show()
 
     # 线性拟合函数
@@ -187,25 +215,25 @@ def main(parameters_list):
         # Al_1
         pl11 = ax.scatter(x_testing_[:, 0], y_testing_[:, y_index], s=15)
         fitting_w1, fitting_b1 = linear_fitting(x_testing_[0:153, 0],
-                                                    y_testing_[0:153, y_index])
+                                                y_testing_[0:153, y_index])
         pl12, = ax.plot(x_phase, fitting_w1 * x_phase +
                         fitting_b1, linestyle='dashed', linewidth=2)
         # Al_2
         pl21 = ax.scatter(x_testing_[:, 1], y_testing_[:, y_index], s=15)
         fitting_w2, fitting_b2 = linear_fitting(x_testing_[:, 1],
-                                                    y_testing_[:, y_index])
+                                                y_testing_[:, y_index])
         pl22, = ax.plot(x_phase, fitting_w2 * x_phase +
                         fitting_b2, linestyle='dashed', linewidth=2)
         # Si
         pl31 = ax.scatter(x_testing_[:, 2], y_testing_[:, y_index], s=15)
         fitting_w3, fitting_b3 = linear_fitting(x_testing_[:, 2],
-                                                    y_testing_[:, y_index])
+                                                y_testing_[:, y_index])
         pl32, = ax.plot(x_phase, fitting_w3 * x_phase +
                         fitting_b3, linestyle='dashed', linewidth=2)
         # Al2Si2Sr
         pl41 = ax.scatter(x_testing_[:, 3], y_testing_[:, y_index], s=15)
         fitting_w4, fitting_b4 = linear_fitting(x_testing_[:, 3],
-                                                    y_testing_[:, y_index])
+                                                y_testing_[:, y_index])
         pl42, = ax.plot(x_phase, fitting_w4 *
                         x_phase + fitting_b4, linestyle='dashed', linewidth=2)
         label11 = r'$\alpha$-(Al)'
@@ -217,14 +245,15 @@ def main(parameters_list):
         label41 = 'Al${_2}$Si${_2}$Sr'
         label42 = 'w${_4}$ = %.2f  b${_4}$ = %.2f' % (fitting_w4, fitting_b4)
         l1 = ax.legend([pl11, pl21, pl31, pl41], [label11, label21, label31, label41],
-                        loc='upper left', frameon=False)
+                       loc='upper left', frameon=False)
         l2 = ax.legend([pl12, pl22, pl32, pl42], [label12, label22, label32, label42],
-                        loc='upper right', frameon=False)
+                       loc='upper right', frameon=False)
         fig.gca().add_artist(l1)
         plt.savefig(path + 'phase_%s_allRE.png' % item)
         linear_coef_allRE = pd.DataFrame(data=np.ones((4, 2)),
-                                        index=['Al_1', 'Al_2', 'Si', 'AlSi2Sr'],
-                                        columns=['weight', 'bias'])
+                                         index=['Al_1', 'Al_2',
+                                                'Si', 'AlSi2Sr'],
+                                         columns=['weight', 'bias'])
         linear_coef_allRE.iloc[0, 0] = fitting_w1
         linear_coef_allRE.iloc[0, 1] = fitting_b1
         linear_coef_allRE.iloc[1, 0] = fitting_w2
@@ -299,15 +328,17 @@ def main(parameters_list):
             print('==================Prediction complete===================')
 
             # 数据可视化(散点图)
-            draw_scatter(EL_Sr.numpy(), y_UTS.numpy(),
-                         EL_Sr_predicting.numpy(), y_predicting.numpy()[:, 0],
-                         'UTS / MPa', training_accuracy[0], testing_accuracy[0], error)
-            draw_scatter(EL_Sr.numpy(), y_YS.numpy(),
-                         EL_Sr_predicting.numpy(), y_predicting.numpy()[:, 1],
-                         'YS / MPa', training_accuracy[1], testing_accuracy[1], error)
-            draw_scatter(EL_Sr.numpy(), y_EL.numpy(),
-                         EL_Sr_predicting.numpy(), y_predicting.numpy()[:, 2],
-                         'EL / %', training_accuracy[2], testing_accuracy[2], error)
+            draw_scatter(EL_Sr.numpy(), y_list.numpy(),
+                         EL_Sr_predicting.numpy(), y_predicting.numpy(), error)
+            # draw_scatter(EL_Sr.numpy(), y_UTS.numpy(),
+            #              EL_Sr_predicting.numpy(), y_predicting.numpy()[:, 0],
+            #              'UTS / MPa', training_accuracy[0], testing_accuracy[0], error)
+            # draw_scatter(EL_Sr.numpy(), y_YS.numpy(),
+            #              EL_Sr_predicting.numpy(), y_predicting.numpy()[:, 1],
+            #              'YS / MPa', training_accuracy[1], testing_accuracy[1], error)
+            # draw_scatter(EL_Sr.numpy(), y_EL.numpy(),
+            #              EL_Sr_predicting.numpy(), y_predicting.numpy()[:, 2],
+            #              'EL / %', training_accuracy[2], testing_accuracy[2], error)
 
             # 综合力学性能计算及可视化
             # data_process(path, EL_Si_predicting.numpy(), EL_Mg_predicting.numpy())
